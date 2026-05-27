19cf39a3252baad5f54f779aef27e0016b1a9d25
Huy Lieu <hlieu7807@gmail.com>
2026-05-27 13:29:28 -0700

    Add remaining files

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
 #prints hello world
 print("Hello, my name is Huy.")
+
+response = input("What is your name? ")
+
+print("Hello " + response)
---
+++ lists.py
@@ -0,0 +1,9 @@
+mixed_list = [42, "hello", 3.14, True, None]
+
+fruits = ["apple", "banana", "cherry"]
+
+mixed_list.append("new item")
+fruits.append("orange")
+
+print(mixed_list)
+print(fruits)
